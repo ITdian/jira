bb83b97dd8b49e34dd77792aece5ae16607676f3
itdian <chenjinbode@yeah.net>
2021-09-03 15:28:50 +0800

    style: 安装emotion 优化登录注册界面

--- src/screens/project-list/search-panel.tsx
+++ src/screens/project-list/search-panel.tsx
@@ -1,3 +1,4 @@
+import { Form, Input, Select } from "antd";
 export interface User {
   id: string;
   name: string;
@@ -16,8 +17,8 @@ interface SearchPanelProps {
 export const SearchPanel = ({ users, param, setParam }: SearchPanelProps) => {
   return (
     <form>
-      <div>
-        <input
+      <div style={{ display: "flex", justifyContent: "center" }}>
+        <Input
           type="text"
           value={param.name}
           onChange={(evt) =>
@@ -27,22 +28,22 @@ export const SearchPanel = ({ users, param, setParam }: SearchPanelProps) => {
             })
           }
         />
-        <select
+        <Select
           value={param.personId}
-          onChange={(evt) =>
+          onChange={(value) =>
             setParam({
               ...param,
-              personId: evt.target.value,
+              personId: value,
             })
           }
         >
-          <option value={""}>负责人</option>
+          <Select.Option value={""}>负责人</Select.Option>
           {users.map((user) => (
-            <option key={user.id} value={user.id}>
+            <Select.Option key={user.id} value={user.id}>
               {user.name}
-            </option>
+            </Select.Option>
           ))}
-        </select>
+        </Select>
       </div>
     </form>
   );
